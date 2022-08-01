ac6c83f76100764b1fdff20263e43c5c5236a324
Julia <noczynska.julia@gmail.com>
2022-08-01 16:33:47 +0100

    change findViewById to bindings

--- app/src/main/java/com/example/applicanttrackingsystem/StartActivity.kt
+++ app/src/main/java/com/example/applicanttrackingsystem/StartActivity.kt
@@ -3,28 +3,31 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.applicanttrackingsystem.databinding.ActivityStartBinding
 import com.example.applicanttrackingsystem.ui.login.LoginActivity
 
 class StartActivity : AppCompatActivity() {
-    private lateinit var buttonCompanies : Button
-    private lateinit var buttonFreelancers : Button
+    private lateinit var binding : ActivityStartBinding
+    private lateinit var companiesButton : Button
+    private lateinit var freelancersButton : Button
     private lateinit var newActivity : Intent
     private lateinit var newBundle : Bundle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        buttonCompanies = findViewById(R.id.start_for_companies_button)
-        buttonFreelancers = findViewById(R.id.start_for_freelancers_button)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        companiesButton = binding.startForCompaniesButton
+        freelancersButton = binding.startForFreelancersButton
         newActivity = Intent(this,LoginActivity::class.java)
         newBundle = Bundle()
 
-        buttonCompanies.setOnClickListener {
+        companiesButton.setOnClickListener {
             newBundle.putString("type","company")
             newActivity.putExtras(newBundle)
             startActivity(newActivity)
         }
-        buttonFreelancers.setOnClickListener {
+        freelancersButton.setOnClickListener {
             newBundle.putString("type","freelancer")
             newActivity.putExtras(newBundle)
             startActivity(newActivity)
